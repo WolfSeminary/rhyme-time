9df94ea64c455d955d808919c869e5e99c5443e7
hitmachut <hitmachut@students.local>
2021-12-28 16:49:01 +0200

    add text field to write a song

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
-
+import WriteSong from './WriteSong';
 function App() {
   return (
     <div className="App">
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
