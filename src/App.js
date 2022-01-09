56ebf50d5ee7071aaee5f82766823f89c5fd63d5
sariYagelnik <sariyagelnik@gmail.com>
2022-01-09 11:45:36 +0200

    feat(App): Set header upper-case

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. Deploy-work!
+          Welcome to Rhyme-Time
         </p>
       </header>
     </div>
