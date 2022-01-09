cb1f13fdb454c3cdce25a6ed78554bcd51ec8f0c
sariYagelnik <sariyagelnik@gmail.com>
2022-01-09 12:02:17 +0200

    remove

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to Rhyme-Time!
+          Welcome to Rhyme-Time
         </p>
       </header>
     </div>
