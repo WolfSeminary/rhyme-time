85673fbd8554a7d755a8f20839d1d653e8c5a47a
saraCashan <3231114sara@gmail.com>
2022-01-11 20:52:41 +0200

    order format page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
-import WriteSong from './WriteSong';
+import AppBarPage from './AppBar';
+
 function App() {
   return (
     <div className="App">
-
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. Deploy-work!
-        </p>
-      </header>
+      <AppBarPage />
     </div>
   );
 }
--- src/WriteSong.js
+++ src/WriteSong.js
@@ -1,39 +1,34 @@
-import * as React from 'react';
+import React from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 
 export default function MultilineTextFields() {
   const [value, setValue] = React.useState('Controlled');
-
   const handleChange = (event) => {
     setValue(event.target.value);
   };
-
   return (
-    <div style={{display:"flex"}}>
-    <Box
-      component="form"
-      sx={{
-        '& .MuiTextField-root': { m: 1, width: '50ch' },
-      }}
-      noValidate
-      autoComplete="off"
-      fullWidth
-     
-    
-     >
-      <div >
-       
-        <TextField
-          id="outlined-multiline-static"
-          label="Write Song"
-          multiline
-          fullWidth
-          rows={4}
-          defaultValue="Write the Song"
-        />
-      </div>
-     
-    </Box></div>
+    <div style={{ display: "flex" }}>
+      <Box
+        component="form"
+        sx={{
+          '& .MuiTextField-root': { m: 1, width: '50ch' },
+        }}
+        noValidate
+        autoComplete="off"
+        fullWidth
+      >
+        <div>
+          <TextField
+            id="outlined-multiline-static"
+            label="Write Song"
+            multiline
+            fullWidth
+            rows={4}
+            defaultValue="Write the Song"
+          />
+        </div>
+      </Box></div>
   );
+
 }
