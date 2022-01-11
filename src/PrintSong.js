ea744b1b34f332ad6bdec8bb3597a13cdd47acbf
rutStern <96536810+rutStern@users.noreply.github.com>
2022-01-11 20:46:51 +0200

    feat(PrintSong):button to print designer song (#11)
    * button to print designer song
    
    * order page format

---
+++ src/PrintSong.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Button from '@mui/material/Button';
+
+const PrintSong = () => {
+  return (
+    <Button variant="contained" disableElevation>
+      Print Designed Song
+    </Button>
+  );
+}
+export default PrintSong
