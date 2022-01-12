f1e99664b0d5c27316ab5404d6b8489a6456c120
shoshiKatsanelbogen <96009965+shoshiKatsanelbogen@users.noreply.github.com>
2022-01-12 12:19:35 +0200

    feat(FetchMoreRhymes):add ButtonFetchMoreeRhymes btn (#25)

---
+++ src/FetchMoreRhymes.js
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+
+export default function FetchMoreRhymes() {
+    return (
+        <Button variant="contained">Fetch more Rhymes</Button>
+    );
+}
