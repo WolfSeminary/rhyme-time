7fe9d6e8057aefcabbb383e7d6ce128756872cc8
saraCashan <3231114sara@gmail.com>
2022-01-12 13:42:23 +0200

    add state num of syllables and set state

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import AppBarPage from './AppBar';
-
+import NumOfSyllables from './NumOfSyllables ';
 function App() {
   return (
     <div className="App">
       <AppBarPage />
+      <NumOfSyllables />
     </div>
   );
 }
