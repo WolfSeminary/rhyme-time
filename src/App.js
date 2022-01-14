10934cd55c2d65e256ecb709ec2a90cc80c6c371
rutStern <ruths00540@gmail.com>
2022-01-14 12:21:45 +0200

    add a song state

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import AppBarPage from './AppBar';
+import MultilineTextFiel from './WriteSong';
 
 function App() {
   return (
     <div className="App">
       <AppBarPage />
+      <MultilineTextFiel/>
     </div>
   );
 }
