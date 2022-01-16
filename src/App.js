4e07308b8e3f0c6a8d5726475617c87b32d521eb
ayala-feld <ayala124274@gmail.com>
2022-01-16 14:37:29 +0200

    Init State

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import AppBarPage from './AppBar';
 import NumOfSyllables from './NumOfSyllables ';
+import ThankYouModal from './ThankYouModal';
 function App() {
   return (
     <div className="App">
       <AppBarPage />
+     <ThankYouModal/>
     </div>
   );
 }
--- src/PrintSong.js
+++ src/PrintSong.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Button from '@mui/material/Button';
-
+import ThankYouModal from './ThankYouModal'
 const PrintSong = () => {
-  return (
+  const [shouldShowThankYouModal,setShouldShowThankYouModal]=useState(false);
+  return (<>
     <Button variant="contained" disableElevation>
       Print Designed Song
     </Button>
-  );
+   {shouldShowThankYouModal && <ThankYouModal/>}
+    </>);
 }
 export default PrintSong
