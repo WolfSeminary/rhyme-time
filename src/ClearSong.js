c7946349f03aed82612e198ae8114e2202e0eedc
shulamitShulman <s0548543873@gmail.com>
2022-01-16 14:07:05 +0200

    tftfhvmnn

--- src/ClearSong.js
+++ src/ClearSong.js
@@ -1,9 +1,6 @@
 import * as React from 'react';
 import IconButton from '@mui/material/IconButton';
 import Stack from '@mui/material/Stack';
-import DeleteIcon from '@mui/icons-material/Delete';
-import AlarmIcon from '@mui/icons-material/Alarm';
-import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
 import ClearIcon from '@mui/icons-material/Clear';
 
 export default function IconButtons() {
@@ -12,7 +9,6 @@ export default function IconButtons() {
       <IconButton aria-label="Clear">
         <ClearIcon />
       </IconButton>
-   
     </Stack>
   );
 }
