1320fc990fd85232377d2657e8de738e54f5012a
chaniFisher <96010140+fisher-chana@users.noreply.github.com>
2022-01-16 14:28:56 +0200

    On print song click (#52)
    * feat(event):add onClick event to the print song buttomn
    
    * feat(event):add onClick event to the print song buttomn

--- src/PrintSong.js
+++ src/PrintSong.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from '@mui/material/Button';
 
 const PrintSong = () => {
+  const [shouldShowThankYouModal, setShouldShowThankYouModal] = useState(false)
+  const onPrintSongClick = () => {
+    setShouldShowThankYouModal(true)
+  }
   return (
-    <Button variant="contained" disableElevation>
+    <Button variant="contained" disableElevation onClick={onPrintSongClick}>
       Print Designed Song
     </Button>
   );
