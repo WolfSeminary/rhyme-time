58b46c7fcf3317b278fb9bae44f5ae05677e6adb
naamafrank1 <naama05567722752gmail.com>
2022-01-16 14:09:17 +0200

    AddUseState

---
+++ src/FetchMoreRhymes.js
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import { useState } from 'react';
+
+export default function FetchMoreRhymes() {
+    const [currentRhymesRange, setcurrentRhymesRange] = useState(0)
+    const fetchRhymes = () => { setcurrentRhymesRange( currentRhymesRange + 10) }
+    return (
+        <Button variant="contained">Fetch more Rhymes</Button>
+    );
+}
--- src/component.js
+++
@@ -1,8 +0,0 @@
-import React, { useState } from "react";
-const componemt = () => {
-    const [currentRhymesRange, setcurrentRhymesRange] = useState(0)
-    const fetchRhymes = () => { setcurrentRhymesRange( currentRhymesRange + 10) }
-    return (<></>);
-}
-
-export default componemt;
