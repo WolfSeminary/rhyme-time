bedbe0ba5090239ebc76bd0d2229a2724a523b47
saraCashan <96009916+saraCashan@users.noreply.github.com>
2022-01-16 14:04:54 +0200

    orderthecode (#51)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import AppBarPage from './AppBar';
-import NumOfSyllables from './NumOfSyllables ';
+
 function App() {
   return (
     <div className="App">
--- src/NumOfSyllables .js
+++ src/NumOfSyllables .js
@@ -1,12 +1,12 @@
-
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import { useState } from 'react';
+
 export default function NumOfSyllables() {
-  const [numOfSyllables,setNumOfSyllables]=useState(0);
-  const onNumOfSyllablesChange=(e)=>{
-         setNumOfSyllables(e.target.value);
+  const [numOfSyllables, setNumOfSyllables] = useState(0);
+  const onNumOfSyllablesChange = (e) => {
+    setNumOfSyllables(e.target.value);
   }
   return (
     <Box
@@ -17,7 +17,7 @@ export default function NumOfSyllables() {
       noValidate
       autoComplete="off"
     >
-      <TextField id="outlined-basic" label="Number of Syllables" variant="outlined" type="number" value={numOfSyllables} onChange={onNumOfSyllablesChange}/>
+      <TextField id="outlined-basic" label="Number of Syllables" variant="outlined" type="number" value={numOfSyllables} onChange={onNumOfSyllablesChange} />
     </Box>
   );
 }
