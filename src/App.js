2dd4908b1f72c7a60367f75ec2ad995b147d23be
naamafrank1 <naama0556772275>
2022-01-18 18:23:11 +0200

    conflit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import WordToRhyme from './WordToRhyme';
 import './App.css';
 import AppBarPage from './AppBar';
 
@@ -7,7 +6,7 @@ import AppBarPage from './AppBar';
 function App() {
   return (
     <div className="App">
-      <AppBarPage />     
+      <AppBarPage />        
     </div>
   );
 }
