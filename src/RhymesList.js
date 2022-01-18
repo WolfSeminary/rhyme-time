afcc63772100be6a6d7e6d820c937c6ad00106f5
shoshiKatsanelbogen <shoshi41207@gmail.com>
2022-01-18 22:20:52 +0200

    copyTextToClipboard

--- src/RhymesList.js
+++ src/RhymesList.js
@@ -4,11 +4,12 @@ import ListItem from '@mui/material/ListItem';
 import ListItemText from '@mui/material/ListItemText';
 import CommentIcon from '@mui/icons-material/Comment';
 import IconButton from '@mui/material/IconButton';
-//import ContentCopyIcon from '@mui/material/IconButton';
 import ContentCopyIcon from '@mui/icons-material/ContentCopy';
-//import {  Comment as CommentIcon } from '@mui/icons-material'
 
 export default function RhymesList(props) {
+  const onCopyRhymeClick=(currentRhyme)=>{
+    navigator.clipboard.writeText(currentRhyme);
+  }
   return (
     <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
       {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((value,index) => (
@@ -21,7 +22,7 @@ export default function RhymesList(props) {
             </IconButton>
           }
         >
-          <ListItemText primary={`${value} ${props.rhymes[index]} `} />
+          <ListItemText primary={`${value} ${props.rhymes[index]} `} onClick={onCopyRhymeClick(props.rhymes[index])}/>
         </ListItem>
       ))}
     </List>
