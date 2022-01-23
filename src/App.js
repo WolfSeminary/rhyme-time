e1f2a07c5308643e67fab3d2eced1a8077ec69f5
yaelShamay <yael0556784292@gmail.com>
2022-01-23 13:44:09 +0200

    sss

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <AppBarPage />
-      <MultilineTextFiel/>
+      <MultilineTextFiel />
     </div>
   );
 }
--- src/ClearSong.js
+++ src/ClearSong.js
@@ -1,12 +1,17 @@
 import * as React from 'react';
+import { useState } from "react";
 import IconButton from '@mui/material/IconButton';
 import Stack from '@mui/material/Stack';
 import ClearIcon from '@mui/icons-material/Clear';
 
 export default function IconButtons() {
+  let [song, setSong] = useState("")
+  const onClearSongClick = () => {
+    setSong("");
+  }
   return (
     <Stack direction="row" spacing={1}>
-      <IconButton aria-label="Clear">
+      <IconButton aria-label="Clear" onClick={onClearSongClick}>
         <ClearIcon />
       </IconButton>
     </Stack>
