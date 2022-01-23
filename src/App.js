ad126291c61f0a4c5ea746fea64b5c73cdf0c939
yaelShamay <yael0556784292@gmail.com>
2022-01-23 13:51:57 +0200

    kkk

--- src/App.js
+++ src/App.js
@@ -7,12 +7,7 @@ import HomepPage from './HomePage';
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-      <AppBarPage />
-      <MultilineTextFiel />
-=======
       <HomepPage />
->>>>>>> 4eb12864c03122bd39cd117a94a4cf3bb6c21914
     </div>
   );
 }
